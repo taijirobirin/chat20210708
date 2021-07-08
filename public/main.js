fc4cddd114b3fd13ebd2c1623a46f5efff90a47e
Yamamoto Taijiro <taijiro@tama.net>
2021-07-09 08:58:48 +0900

    何か入力しないと送信できないようにした

--- public/main.js
+++ public/main.js
@@ -4,8 +4,10 @@ const input = document.getElementById("msg");
 const chats = document.getElementById("chats");
 
 form.addEventListener('submit', function(event){
-  socketio.emit('message', input.value);
-  input.value='';
+  if(input.value!==''){
+    socketio.emit('message', input.value);
+    input.value='';
+  }
   event.preventDefault();
 })
 socketio.on('message',function(msg){
